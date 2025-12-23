d898c02f1aef7a3088918e2dbd54b373283fa00c
Dustin L. Howett <dustin@howett.net>
2025-12-22 17:42:22 -0800

    Add Profilarr

---
+++ ct/profilarr.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: DHowett
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/Dictionarry-Hub/prrofilarr
+
+APP="Profilarr"
+var_tags="${var_tags:-arr;media}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-512}"
+var_disk="${var_disk:-2}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d /opt/profilarr/ ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if check_for_gh_release "profilarr" "Dictionarry-Hub/profilarr"; then
+    msg_info "Stopping Services"
+    systemctl stop profilarr
+    msg_ok "Services Stopped"
+
+    PYTHON_VERSION="3.9" setup_uv
+    fetch_and_deploy_gh_release "profilarr" "Dictionarry-Hub/profilarr" "tarball" "latest" "/opt/profilarr"
+
+    msg_info "Updating Profilarr"
+    cd /opt/profilarr
+    $STD uv venv /opt/profilarr/backend/.venv
+    $STD /opt/profilarr/backend/.venv/bin/python -m ensurepip --upgrade
+    $STD /opt/profilarr/backend/.venv/bin/python -m pip install --upgrade pip
+    $STD /opt/profilarr/backend/.venv/bin/python -m pip install -r /opt/profilarr/backend/requirements.txt
+    chmod -R 755 /opt/profilarr
+    msg_ok "Updated Profilarr"
+
+    msg_info "Starting Services"
+    systemctl start profilarr
+    msg_ok "Services Started"
+    msg_ok "Updated successfully!"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:6868${CL}"
---
+++ install/profilarr-install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: DHowett
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/Dictionarry-Hub/profilarr
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+PYTHON_VERSION="3.9" setup_uv
+fetch_and_deploy_gh_release "profilarr" "Dictionarry-Hub/profilarr" "tarball" "latest" "/opt/profilarr"
+msg_info "Installing Profilarr"
+cd /opt/profilarr
+$STD apt install -yy git
+$STD uv venv /opt/profilarr/backend/.venv
+$STD /opt/profilarr/backend/.venv/bin/python -m ensurepip --upgrade
+$STD /opt/profilarr/backend/.venv/bin/python -m pip install --upgrade pip
+$STD /opt/profilarr/backend/.venv/bin/python -m pip install -r /opt/profilarr/backend/requirements.txt
+chmod -R 755 /opt/profilarr
+mkdir /config
+chmod -R 755 /config
+msg_ok "Installed Profilarr"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/profilarr.service
+[Unit]
+Description=Profilarr
+Wants=network-online.target
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/profilarr
+ExecStart=/opt/profilarr/backend/.venv/bin/gunicorn --bind 0.0.0.0:6868 --timeout 600 app.main:create_app()
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now profilarr
+msg_ok "Configured Service"
+
+motd_ssh
+customize
+cleanup_lxc
